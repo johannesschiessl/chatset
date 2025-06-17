a4eb9e75416fca8b2c40a54ae34c0b0c5236a3a8
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2025-06-17 19:04:07 +0200

    add auth check

--- src/components/app-sidebar-wrapper.tsx
+++ src/components/app-sidebar-wrapper.tsx
@@ -1,8 +1,15 @@
 import { preloadQuery } from "convex/nextjs";
 import { api } from "../../convex/_generated/api";
 import { AppSidebar } from "./app-sidebar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 export default async function AppSidebarWrapper() {
-  const preloadedChats = await preloadQuery(api.chats.getChatsGroupedByDate);
+  const authData = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const preloadedChats = await preloadQuery(api.chats.getChatsGroupedByDate, {
+    sessionToken: authData?.session.token ?? "",
+  });
   return <AppSidebar preloadedChats={preloadedChats} />;
 }
